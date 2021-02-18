18a0908b96884243f8df66266d912929b25c66fc
ammcavoy <adam@mcavoys.org>
2021-02-17 23:15:44 -0600

    Update Parser

--- skeleton_parser2.py
+++ skeleton_parser2.py
@@ -29,6 +29,12 @@ import sys
 from json import loads
 from re import sub
 
+#create .dat file object for each database
+sellers_file = open('sellers.dat', 'w') # called in parseSeller()
+bids_file    = open('bids.dat'   , 'w') # called in parseBids()
+bidders_file = open('bidders.dat', 'w') # called in parseBidders()
+items_file   = open('items.dat'  , 'w') # called in parseItems()
+
 columnSeparator = "|"
 
 # Dictionary of months used for date transformation
@@ -78,19 +84,103 @@ def parseJson(json_file):
     with open(json_file, 'r') as f:
         items = loads(f.read())['Items'] # creates a Python dictionary of Items for the supplied json file
         for item in items:
+            #"required": ["ItemID", "Name", "Category", "Currently", "First_Bid", "Number_of_Bids", "Bids", "Location", "Country", "Started", "Ends", "Seller", "Description"]
+            print(
+                item['ItemId']  + 
+                columnSeparator + item['UserId'] + 
+                columnSeparator + item['Country'] + 
+                columnSeparator + item['Location'] +
+                columnSeparator + item['Name'] + 
+                columnSeparator + transformDollar(item['Buy_Price']) +   #might not be in JSON
+                columnSeparator + transformDollar(item['First_Bid'] + 
+                columnSeparator + transformDollar(item['Currently']) + 
+                columnSeparator + items['Number_of_Bids']) +
+                columnSeparator + items['Description'] + 
+                columnSeparator + transformDttm(items['Started'] + 
+                columnSeparator + transformDttm(items['Ends']) + 
+                columnSeparator + CATEGORIES!!!!!
             
+            parseBids(item['Bids'], item['ItemID'])
+            parseSeller(item['Seller'], item['Country'], item['Location'])
             """
             TODO: traverse the items dictionary to extract information from the
             given `json_file' and generate the necessary .dat files to generate
             the SQL tables based on your relation design
             """
             pass
+        
+
+
+
+"""
+Bidder dictionary containing all the content for each bidder
+    this one is correct and done - think so
+"""
+def parseSeller(seller, country, location):
+    sys.stdout = seller_file #redirects output stream to correct dat file
+    print(
+        seller['UserID'] +
+        columnSeparator + seller['Rating'] + 
+        columnSeparator + country + 
+        columnSeparator + location)
+    return
+
+"""
+Bids is an array containing items
+Bids{description:"Bids placed on the item", type:array, items}
+    items{title:Bid, type:object, properties}
+        properties{Bidder,Time,Amount}
+            Bidder{description,type:object, properties, required}
+                properties{Location,Country,UserID,Rating}
+                    Loaction{description,type,minimum}
+                    Country{description,type,minimum}
+                    UserID{description, type}
+                    Rating{description, type}
+                required{userID, Rating}
+            Time{description, type:string}
+            Amount{description,type:string}
+"""
+def parseBids(bids, itermID):
+    sys.stdout = bids_file #redirects output stream to correct dat file
+    if bids is None:
+        placeNullNotNone()
+        return
+    for bid in bids:
+        print(ItemId + columnSeparator + bid['Bid']['Bidder']['UserID'] + columnSeparator + 
+            transformDollar(bid['Bid']['Amount']) + columnSeparator + transformDttm(bid['bid']['Time']))
+        parseBidder(bid['Bid']['Bidder'])
+        # print(bids)
+    return
+
+"""
+Bidder dictionary containing all the content for each bidder
+"""
+def parseBidder(bidder):
+    sys.stdout = bidder_file #redirects output stream to correct dat file
+    if bidder['Location'] is None:
+        placeNullNotNone()
+    if bidder['Country'] is None:
+        placeNullNotNone() 
+    print(bidder['UserID'] + columnSeparator + bidder['Rating']) 
+    
+    return
+
+""" 
+replaces "none" item fields in the JSON file with the string "NULL" 
+see Loading NULL Values in the bulk-loading.pdf
+"""
+def placeNullNotNone():
+    print("NULL")
+    print(columnSeparator)
+    return
 
 """
 Loops through each json files provided on the command line and passes each file
 to the parser
 """
 def main(argv):
+
+
     if len(argv) < 2:
         print('Usage: python skeleton_json_parser.py <path to json files>', file=sys.stderr)
         sys.exit(1)
